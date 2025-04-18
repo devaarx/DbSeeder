7d38c2333aac4a00de3c718b93096b2573203ba8
devaarx <heydevdeep@proton.me>
2025-04-18 20:47:32 +0530

    update wrangler

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tailwindcss(), cloudflare()],
+  plugins: [svelte(), tailwindcss()],
 })
